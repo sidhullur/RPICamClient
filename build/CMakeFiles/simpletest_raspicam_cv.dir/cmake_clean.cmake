aaa43fbe19e23077caa57cf4a27030929f3d648d
Siddhant Hullur <51142971+sidhullur12@users.noreply.github.com>
2022-09-03 02:16:37 +0100

    initial commit

---
+++ build/CMakeFiles/simpletest_raspicam_cv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simpletest_raspicam_cv.dir/simpletest_raspicam_cv.cpp.o"
+  "simpletest_raspicam_cv"
+  "simpletest_raspicam_cv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simpletest_raspicam_cv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ simpletest_raspicam_cv.cpp
@@ -0,0 +1,59 @@
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <raspicam/raspicam_cv.h>
+#include <unistd.h>
+
+#include <arpa/inet.h>
+#include <sys/socket.h>
+using namespace std; 
+
+#define IP "192.168.86.5"
+#define PORT 8123
+
+int main ( int argc,char **argv ) {
+	int sock = 0, valread, client_fd;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = { 0 };
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+  
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+  
+    // Convert IPv4 and IPv6 addresses from text to binary
+    // form
+    if (inet_pton(AF_INET, IP, &serv_addr.sin_addr)
+        <= 0) {
+        printf(
+            "\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+  
+    if ((client_fd
+         = connect(sock, (struct sockaddr*)&serv_addr,
+                   sizeof(serv_addr)))
+        < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+	}
+	raspicam::RaspiCam_Cv Camera;
+	Camera.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+	Camera.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+	
+	if (!Camera.open()) {cerr<<"Error opening the camera"<<endl;return -1;}
+	usleep(3000000);
+	cv::Mat image;
+	Camera.grab();
+	int  imgSize = 640*480*3;
+	while(true) {
+		Camera.retrieve(image);
+		send(sock, image.data, imgSize, 0);
+		Camera.grab();
+	}
+	
+	Camera.release();
+	return 0;
+}
